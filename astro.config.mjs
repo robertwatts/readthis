b93e7fd6ac36e2980042171fbf8eba8de6960859
Robert Watts <robw@wonderbly.com>
2025-01-20 12:17:11 +0000

    feat: use vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
